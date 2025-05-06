8581ebf1e5db2003bc89d5b36e07eaa6d5d42bab
Jailton <87905436+Jailton-Silva@users.noreply.github.com>
2025-05-06 17:20:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ if ($cadastro_ativo_devido_erro || isset($_GET['mostrar_erro_cadastro'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EchoSphere Auth</title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="./style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Geist+Sans:wght@400;500;700&display=swap" rel="stylesheet">
